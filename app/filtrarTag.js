36997d404a636f84e9b8f3a79d015768e2dc2b90
linofir <firmanoster@gmail.com>
2023-04-28 14:03:13 -0300

    event listener refatorado, funcionalidade de filtrarPreco implementada

--- app/filtrarTag.js
+++ app/filtrarTag.js
@@ -2,41 +2,78 @@
 const botaoTagFrontEnd = document.querySelector("#btnFiltrarLivrosFront")
 const botaoTagBackEnd = document.querySelector("#btnFiltrarLivrosBack")
 const botaoTagDados = document.querySelector("#btnFiltrarLivrosDados")
+const botaoTagQuantidade = document.querySelector("#btnLivrosDisponiveis")
 
-botaoTagFrontEnd.addEventListener('click',(e) => {
-    console.log(e.target.value) 
-    filtrarTags(livros, e.target.value)
-})
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach((b) => b.addEventListener('click', (e) => {
+    filtrarTags(livros, e)
+} ))
 
-botaoTagBackEnd.addEventListener('click',(e) => {
-    console.log(e.target.value) 
-    filtrarTags(livros, e.target.value)
-})
+// primeira versão dos botoes
+// botaoTagFrontEnd.addEventListener('click',(e) => {
+//     console.log(e.target.value) 
+//     filtrarTags(livros, e.target.value)
+// })
 
-botaoTagDados.addEventListener('click',(e) => {
-    console.log(e.target.value) 
-    filtrarTags(livros, e.target.value)
-})
-function filtrarTags(livros, tag) {
+// botaoTagBackEnd.addEventListener('click',(e) => {
+//     console.log(e.target.value) 
+//     filtrarTags(livros, e.target.value)
+// })
+
+// botaoTagDados.addEventListener('click',(e) => {
+//     console.log(e.target.value) 
+//     filtrarTags(livros, e.target.value)
+// })
+
+// botaoTagQuantidade.addEventListener('click',(e) => {
+//     console.log(e.target.value)     
+//     filtrarTags(livros, e.target.value)
+// })
+
+function filtrarTags(livros, evento) {
+    let tagCategoria = evento.target.value
     let livrosDesconto = aplicaDescontoLivros(livros)
+    let listaLivrosAtualizada = livrosDesconto
+    console.log(0, listaLivrosAtualizada)
+    let textoBotoesTags = evento.target.innerText
+    
     const livrosFiltrados = livrosDesconto.filter((livro) => {
-        return livro.categoria === tag
+        return livro.categoria === tagCategoria
     })
     removerLivrosHTML()
     adicionarLivros(livrosFiltrados) 
+    listaLivrosAtualizada = livrosFiltrados
+    console.log(1, listaLivrosAtualizada)
+    
+    if (textoBotoesTags === "LIVROS DISPONÍVEIS") {
+        let livrosFiltradosQuantidade = livrosDesconto.filter((livro) => {
+            return livro.quantidade > 0
+        })
+        removerLivrosHTML()
+        adicionarLivros(livrosFiltradosQuantidade)
+        listaLivrosAtualizada = livrosFiltradosQuantidade 
+        console.log(2, listaLivrosAtualizada)
+    }else if( textoBotoesTags === "ORDENAR POR PREÇO") {
+        console.log(3, livros)
+        organizarPreco(livrosDesconto)
+    }
 }
 
 function removerLivrosHTML() {
+    //forma de apagar e refazer todo filtro
+    //elementoSecaoLivros.innerHTML = ""
     let listaElementosRemovidos = document.querySelectorAll(".livro")
-    let nodeRemovidos = elementoSecaoLivros.childNodes
-    let constrole = nodeRemovidos.length
-    //descobris como deletar pela nodelist foreach
+    //let nodeRemovidos = elementoSecaoLivros.childNodes
+    
     listaElementosRemovidos.forEach((livro) => {
         let elementoRemovido = document.querySelector(".livro")
         elementoRemovido.remove()
     })
-    // for ( let i = 2; i <= constrole; i++) {
-    //     let elementoRemovido = document.querySelector(".livro")
-    //     elementoRemovido.remove()
-    // }
+}
+
+//os filtros não está sendo agregador
+function organizarPreco(livros) {
+    let livrosFiltradosPreco = livros.sort((a,b) => a.preco - b.preco)
+    removerLivrosHTML()
+    adicionarLivros(livrosFiltradosPreco)
 }
